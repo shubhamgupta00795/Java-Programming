94a815a06e8e60012537582708cd741fbda94118
Shubham_Gupta <shubhamgupta00795@gmail.com>
2023-02-28 12:47:09 +0530

    Added a test program

---
+++ Day3/strings.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+class strings{
+    public static void main(String[] args){
+        String s1 = "Sample";
+        String[] arr = s1.split("");
+        System.out.print(arr[0]);
+    }
+}
