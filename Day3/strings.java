41c971e8737ec2b12e10bec19eb87487b436f769
Shubham_Gupta <shubhamgupta00795@gmail.com>
2023-03-01 10:46:20 +0530

    Day 3 task completed

--- Day3/strings.java
+++ Day3/strings.java
@@ -1,8 +1,16 @@
 import java.lang.*;
 class strings{
     public static void main(String[] args){
-        String s1 = "Sample";
-        String[] arr = s1.split("");
-        System.out.print(arr[0]);
+        String s1 = "GUGPUAGAFQBMPYAGGAAOALAELGGGAOGLGEGZ";
+        char[] arr = s1.toCharArray();
+        int spfCount=0;
+        for(int i=0;i<s1.length();i++){
+            // System.out.println(arr[i]=='G');
+            if(s1.charAt(i)=='G'){
+                spfCount++;
+            }
+
+        }
+        System.out.print(spfCount);
     }
 }
